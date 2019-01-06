f94758a58ff2d8053ff409421ecf367a2bf8acf5
Stewart ilondanga <stilondanga@gmail.com>
2019-01-06 14:16:46 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,21 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+const url = document.getElementById('url')
+const width = document.getElementById('width')
+const height = document.getElementById('height')
+
+url.addEventListener('input', calculate)
+width.addEventListener('input', calculate)
+height.addEventListener('input', calculate)
+
+function calculate(e) {
+	const id = url.value.split('/').pop()
+	const src = `https://source.unsplash.com/${id}/${width.value}x${height.value}`
+	document.querySelector('a').textContent = src
+	document.querySelector('a').setAttribute('href', src)
+	document.querySelector('img').setAttribute('src', src)
+}
+
+calculate();
